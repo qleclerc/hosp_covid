9b92dba8d9991f3ffac31106b95e4a1a1ec46f23
GwenKnight <gwen.knight@github.com>
2020-03-10 07:42:50 +0000

    original los file

---
+++ main_file.R
@@ -0,0 +1,137 @@
+#### Impact of COVID19 on hospital bed capacity 
+
+library(ggplot2)
+library(reshape2)
+library(patchwork)
+library(gridGraphics)
+### Fills beds for 6 months. Daily time step
+# nbeds = number of beds in ward
+# los = length of stay (mean, sd) for normal / covid patients
+# cov_curve = changing proportion of patients that are covid+
+
+bed_filling <- function(nbeds, los_norm, los_cov, cov_curve){
+  
+  ## Normal patients arriving over 6mo
+  A <- as.data.frame(matrix(0,180,3))
+  colnames(A) <- c("day","norm_admin","cov_admin")
+  A[,"day"] <- seq(1,180,1)
+  A[,"norm_admin"] <- rnorm(180,inc_rate[2],1) #  depends on normal size / LOS of hospital
+
+
+  
+  
+  A <- array(0,c(nbeds,2,180)) # Array - 3D matrix. 
+  colnames(A)<-c("patno","status")
+  #Wn <- array(0,c(nbeds,2,180)) # Array - 3D matrix. 
+  #colnames(Wn)<-c("patno","status")
+  # rows = bed, columns = c(patient number, actual status, presumed status, days in hospital), 3D = time
+  
+  # track number of patients 
+  pat_num <- 0
+  
+  # fill by bed
+  for(i in 1:nbeds){
+    
+    cumlos <- 0
+    
+    # first patient in bed
+    pat_num <- pat_num + 1
+    pat_status <- ifelse(runif(1) < cov_curve[1],1,0) # 1 = COVID positive
+    los <- ceiling(ifelse(pat_status == 1, rnorm(1,los_cov), rnorm(1,los_norm)))
+    
+    A[i,c("patno","status"),1:los] <- c(pat_num,pat_status)
+    
+    cumlos <- cumlos + los
+    
+    while(cumlos < 180){
+      pat_num <- pat_num + 1
+      pat_status <- ifelse(runif(1) < cov_curve[cumlos+1],1,0) # 1 = COVID positive
+      los <- ceiling(ifelse(pat_status == 1, rnorm(1,los_cov), rnorm(1,los_norm)))
+      
+      A[i,c("patno","status"),pmin((cumlos + (1:los)),180)] <- c(pat_num,pat_status)
+      
+      cumlos <- cumlos + los
+    }
+    
+  }
+  return(list(A = A, pat_num = pat_num))
+  
+}
+
+####****************************************************************************************************************
+####****************************************************************************************************************
+####****************************************************************************************************************
+####****************************************************************************************************************
+
+## Admission rate for "normal" patients
+# 180 days (6mo)
+# BASED ON ICHNT
+# Length of stay 8 days
+# 157 ADULT beds
+# 80% full 
+occ_bed_days <- c(0.7,0.8,0.9) * 157 * 180  # number of occupied bed days
+unique_pat <- occ_bed_days / 8 
+inc_rate <- unique_pat / 180 # number arrive every day
+inc_rate # mean and range - make norm around mean + 1 SD
+
+
+
+#### Run
+nbeds <- 157 # ICHNT Critical care beds
+#nbeds <- 4000 # UK ICU
+los_norm <- c(7.9,4) # ICHNT ICU NORMAL LOS 
+
+los_cov <- c(20,8) # GUESS 
+
+# NO COVID
+cov_curve <- seq(0,0,length.out = 180)
+output_nocovid <- bed_filling(nbeds, los_norm, los_cov, cov_curve)
+
+h_noco <- melt(output_nocovid$A,id.vars = "patno")
+colnames(h_noco) <- c("bedno","variable","time","value")
+h_noco <- dcast(h_noco, time + bedno ~variable)
+ggplot(h_noco, aes(x = time, y = bedno) ) + 
+  geom_point(aes(col = factor(status))) +
+  scale_colour_discrete(name  ="Status",breaks=c("0", "1"),labels=c("Normal", "Covid")) 
+
+pcov <- as.data.frame(cbind(seq(1,180,1),cov_curve));
+colnames(pcov)<-c("days","cprev")
+
+# Rapid to 20% then decrease
+cov_curve <- c(seq(0,0.2,length.out = 100),seq(0.2,0,length.out = 80)) # GUESS
+output_to20 <- bed_filling(nbeds, los_norm, los_cov, cov_curve)
+
+h_to20 <- melt(output_to20$A,id.vars = "patno")
+colnames(h_to20) <- c("bedno","variable","time","value")
+h_to20 <- dcast(h_to20, time + bedno ~variable)
+p1 <- ggplot(h_to20, aes(x = time, y = bedno) ) + 
+  geom_point(aes(col = factor(status))) + 
+  scale_colour_discrete(name  ="Status",breaks=c("0", "1"),labels=c("Normal", "Covid")) +
+  xlab("Day") + ylab("Bed number")
+
+
+pcov$cprev <- cov_curve
+p2 <- ggplot(pcov, aes(x=days, y = cprev)) + geom_line() + 
+               scale_x_continuous("Day") + scale_y_continuous("COV19 prevelance at entry") 
+  
+p1/p2
+ggsave(paste0("plots/",nbeds,"_to20.pdf"))
+
+# Rapid to 80% then decrease
+cov_curve <- c(seq(0,0.8,length.out = 100),seq(0.8,0,length.out = 80)) # GUESS
+output_to80 <- bed_filling(nbeds, los_norm, los_cov, cov_curve)
+
+h_to80 <- melt(output_to80$A,id.vars = "patno")
+colnames(h_to80) <- c("bedno","variable","time","value")
+h_to80 <- dcast(h_to80, time + bedno ~variable)
+p1 <- ggplot(h_to80, aes(x = time, y = bedno) ) + 
+  geom_point(aes(col = factor(status))) + 
+  scale_colour_discrete(name  ="Status",breaks=c("0", "1"),labels=c("Normal", "Covid")) + 
+  xlab("Day") + ylab("Bed number")
+
+pcov$cprev <- cov_curve
+p2 <- ggplot(pcov, aes(x=days, y = cprev)) + geom_line() + 
+  scale_x_continuous("Day") + scale_y_continuous("COV19 prevelance at entry")
+
+p1/p2
+ggsave(paste0("plots/",nbeds,"_to80.pdf"))

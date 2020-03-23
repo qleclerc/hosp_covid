00c6c12abb3f7f2c79bdfa98b40cb595fe69add3
qleclerc <quentin.leclerc@lshtm.ac.uk>
2020-03-23 18:26:19 +0100

    pull delay dist by age

--- delay_dist.r
+++ delay_dist.r
@@ -22,3 +22,21 @@ saveRDS(delays, "delay_dist_linelist.rds")
 sample(delays, 10, replace = TRUE)
 
 h <- hist(delays)
+
+
+## filter and format
+df = ll %>%
+  select(ID, age, date_admission_hospital,
+           outcome, date_death_or_discharge) %>%
+  filter(!is.na(age), !is.na(outcome)) %>%
+  mutate(date_death = as.Date(date_death_or_discharge, format = "%d.%m.%Y"),
+         delay = date_death - date_admission_hospital) %>%
+  select(-date_death_or_discharge) %>%
+  mutate(age = as.integer(age)) %>%
+  filter(!is.na(age), !is.na(delay)) %>%
+  mutate(delay = as.integer(delay)) %>%
+  mutate(age = cut(age, breaks = seq(0,100,10)))
+
+ggplot(df, aes(age,delay)) +
+  geom_boxplot() +
+  geom_point()
